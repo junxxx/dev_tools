dfa638a4c36486d8b6dc39687a38e8db008952bf
junxxx <hprjunxxx@gmail.com>
2018-12-04 11:31:44 +0800

    install nginx

--- linux/lnmp/installNginx.sh
+++ linux/lnmp/installNginx.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
-
-useradd nginx
-goupadd nginx 
+app=nginx
+#check group nginx exist 
+if ! grep -E $app /etc/passwd
+then
+    useradd -r nginx -s /bin/false
+fi
+if ! grep -E $app /etc/group
+then
+    groupadd nginx 
+fi
 usermod -a -G nginx nginx
 
-cd /opt/source
+sourceDir=/opt/source
+appDir=/opt/app
+if [ ! -d $sourceDir ]; then
+    mkdir -p $sourceDir
+fi
+if [ ! -d $appDir ]; then
+    mkdir -p $appDir
+fi
+cd $sourceDir
 wget -O nginx.tar.gz http://nginx.org/download/nginx-1.14.1.tar.gz
 tar xvf nginx.tar.gz
 cd nginx-1.14.1 
