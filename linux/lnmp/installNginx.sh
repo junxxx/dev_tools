2f7f20312886dd3065a717c519721e354efe88ba
junxxx <hprjunxxx@gmail.com>
2018-12-04 13:44:25 +0800

    install nginx

--- linux/lnmp/installNginx.sh
+++ linux/lnmp/installNginx.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 app=nginx
-#check group nginx exist 
-if ! grep -E $app /etc/passwd
+appVer=1.14.1
+#check group user nginx exist or not
+if [ -z `grep -E "$app:" /etc/passwd` ]
 then
     useradd -r nginx -s /bin/false
 fi
-if ! grep -E $app /etc/group
+if [ -z `grep -E "$app:" /etc/group` ]
 then
     groupadd nginx 
 fi
 usermod -a -G nginx nginx
-
 sourceDir=/opt/source
 appDir=/opt/app
 if [ ! -d $sourceDir ]; then
@@ -19,10 +19,17 @@ fi
 if [ ! -d $appDir ]; then
     mkdir -p $appDir
 fi
+if [ -f $sourceDir/nginx.tar.gz ]; then
+    wget -O $souceDir/nginx.tar.gz http://nginx.org/download/nginx-$appVer.tar.gz
+fi
 cd $sourceDir
-wget -O nginx.tar.gz http://nginx.org/download/nginx-1.14.1.tar.gz
+if [ -d /opt/app/$app ]; then
+    echo "app /opt/app/$app already exist, install quit."
+    exit(0)
+fi
 tar xvf nginx.tar.gz
-cd nginx-1.14.1 
-./configure --prefix=/opt/app/nginx --user=nginx --group=nginx --with-http_v2_module --with-http_gunzip_module 
+cd nginx-$appVer 
+./configure --prefix=/opt/app/$app --user=nginx --group=nginx --with-http_v2_module --with-http_gunzip_module 
 make
 make install
+rm -rf $sourceDir/nginx.tar.gz $sourceDir/nginx-$appVer
