ce779bca49a4896825487da6292e4e5c74cb4426
junxxx <hprjunxxx@gmail.com>
2022-03-27 17:40:14 +0800

    install tools on mac os

---
+++ mac/install.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+# brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Rime
+brew install --cask squirrel
