62c8655ee190645f4d429f707c6fc539b81b99e2
junxxx <hprjunxxx@gmail.com>
2022-05-16 11:03:16 +0800

    fix autostart script

--- linux/autostart.sh
+++ linux/autostart.sh
@@ -4,7 +4,9 @@ HOME="/home/centos"
 SCRIPTPATH=${HOME}"/scripts"
 APPPATH=${HOME}"/app"
 
-./${SCRIPTPATH}/gosh.sh
-./${SCRIPTPATH}/readnews.sh
-nohup ${APPPATH}/book.creator.server/book.creator.server &
-nohup ${APPPATH}/dockerhub-hook-handler/dockerhub-webhook &
+${SCRIPTPATH}/gosh.sh
+${SCRIPTPATH}/readnews.sh
+nohup ${APPPATH}/book.creator.server/book.creator.server >> \
+${APPPATH}/book.creator.server/nohup.out &
+nohup ${APPPATH}/dockerhub-hook-handler/dockerhub-webhook >> \
+${APPPATH}/dockerhub-hook-handler/nohup.out &
